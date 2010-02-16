59a2f39b05002e0c21423e64c3cce3cb8dd27a89
Lars Wirzenius <liw@liw.fi>
2010-02-16 23:16:50 +1300

    Blah. Save this state even though it does not work, I need to back off and regroup.

--- btree.py
+++ btree.py
@@ -8,9 +8,10 @@ class Node(object):
     
     '''
 
-    def __init__(self, nodeid, pairs):
+    def __init__(self, nodeid, pairs, isleaf):
         self.id = nodeid
         self.pairs = pairs
+        self.isleaf = isleaf
 
     def __len__(self):
         '''Return number of keys in this node.'''
@@ -24,6 +25,10 @@ class Node(object):
         '''Return all keys in this node.'''
         return [key for key, value in self.pairs]
 
+    def items(self):
+        '''Return list of key/value pairs.'''
+        return self.pairs
+
     def lookup(self, key):
         '''Return value corresponding to a given key in this node.
         
@@ -36,11 +41,94 @@ class Node(object):
                 return value 
         raise KeyError(key)
 
+    def find_next_highest_key(self, key):
+        '''Return smallest key that is bigger than given key.
+        
+        Raise KeyError if not found.
+        
+        '''
+        
+        for k in sorted(self.keys()):
+            if k > key:
+                return k
+        raise KeyError(key)
 
-#class Tree(object):
-#    '''A B-tree variant.'''
-#    def __init__(self, keysize, nodesize):
-#    def insert(self, key, value):
-#    def lookup(self, key):
+    def __contains__(self, key):
+        try:
+            self.lookup(key)
+        except KeyError:
+            return False
+        else:
+            return True
+
+
+class Btree(object):
+
+    '''A B-tree variant.
+    
+    The tree stores key/value pairs, using Node instances (but
+    this is invisible to the user). The tree is balanced.
+    
+    '''
+
+    def __init__(self, nodesize):
+        self.nodesize = nodesize
+        self.height = 0
+        self.latest_nodeid = 0
+        self.root = None
+
+    def new_node(self, old_node, new_pairs):
+        if old_node:
+            pairs = [(key, old_node.lookup(key)) for key in old_node.keys]
+        else:
+            pairs = []
+        pairs += new_pairs
+        pairs.sort()
+        self.latest_nodeid += 1
+        return Node(self.latest_nodeid, pairs)
+
+    def lookup(self, key):
+        '''Return value associated with key or raise KeyError if missing.'''
+        node = self.root
+        while node:
+            if node.isleaf:
+                return node.lookup(key)
+            else:
+                keys = node.keys()
+                if key < keys[0]:
+                    break
+                if key > keys[-1]:
+                    node = node.lookup(keys[-1])
+                else:
+                    next = node.find_next_highest_key(key)
+                    node = node.lookup(next)
+        raise KeyError(key)
+
+    def insert(self, key, value):
+        '''Insert key/value pair into tree.
+        
+        If key already exists in tree, replace it.
+        
+        '''
+
+        if not self.root:
+            self._root = self.new_node(None, [(key, value)])
+        else:
+            self.root = self._insert(self.root, key, value)
+    
+    def _insert(self, node, key, value):
+        if node.size() + len(key) + len(value) < self.nodesize:
+            return self.new_node(node, [(key, value)])
+        else:
+            return self.split_node(node, key, value)
+
+    def split_node(self, node, key, value):
+        pairs = sorted(node.items() + [(key, value)])
+        i = len(pairs) / 2
+        assert i > 0
+        n1 = self.new_node(None, pairs[:i])
+        n2 = self.new_node(None, pairs[i:])
+        return self.new_node(None, [(pairs[0][0], n1), (pairs[i][0], n2)])
+            
 #    def remove(self, key):
 
--- btree_tests.py
+++ btree_tests.py
@@ -6,7 +6,7 @@ import btree
 class EmptyNodeTests(unittest.TestCase):
 
     def setUp(self):
-        self.empty = btree.Node(0, [])
+        self.empty = btree.Node(0, [], True)
 
     def test_has_zero_length(self):
         self.assertEqual(len(self.empty), 0)
@@ -20,6 +20,12 @@ class EmptyNodeTests(unittest.TestCase):
     def test_lookup_raises_error_for_nonexistent_key(self):
         self.assertRaises(KeyError, self.empty.lookup, '0')
         
+    def test_is_leaf(self):
+        self.assert_(self.empty.isleaf)
+
+    def test_does_not_find_higher_key(self):
+        self.assertRaises(KeyError, self.empty.find_next_highest_key, '')
+        
     
 class NodeTests(unittest.TestCase):
 
@@ -27,7 +33,9 @@ class NodeTests(unittest.TestCase):
         self.keys = ['foo', 'bar', 'foobar']
         self.values = ['FOO', 'BAR', 'FOOBAR']
         self.nodeid = 12765
-        self.node = btree.Node(self.nodeid, zip(self.keys, self.values))
+        self.node = btree.Node(self.nodeid, 
+                               zip(self.keys, self.values),
+                               False)
  
     def test_has_right_id(self):
         self.assertEqual(self.node.id, self.nodeid)
@@ -44,3 +52,27 @@ class NodeTests(unittest.TestCase):
         
     def test_finds_value(self):
         self.assertEqual(self.node.lookup('foo'), 'FOO')
+
+    def test_finds_next_highest_key(self):
+        self.assertEqual(self.node.find_next_highest_key('bar'), 'foo')
+        
+    def test_does_not_find_higher_key_than_largest_key(self):
+        self.assertRaises(KeyError, self.node.find_next_highest_key,
+                          sorted(self.keys)[-1])
+
+        
+class BtreeTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tree = btree.Btree(nodesize=16)
+
+    def test_has_zero_height_initially(self):
+        self.assertEqual(self.tree.height, 0)
+        
+    def test_looking_up_nonexistent_key_raises_keyerror(self):
+        self.assertRaises(KeyError, self.tree.lookup, 'a')
+        
+    def test_inserting_key_works(self):
+        self.tree.insert('a', 'foo')
+        self.assertEqual(self.tree.lookup('a'), 'foo')
+
