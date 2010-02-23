b0609a31d1ab56a8874bf951ce8d9a7e270c6325
Lars Wirzenius <liw@liw.fi>
2010-02-23 19:20:46 +1300

    Add start of binary search tree. Realize am doing too complicated stuff for first step.

--- btree.py
+++ btree.py
@@ -10,3 +10,32 @@ class IndexNode(object):
     def __init__(self, child1, child2):
         self.child1 = child1
         self.child2 = child2
+        
+
+class BinarySearchTree(object):
+
+    def __init__(self):
+        self.root = None
+        
+    def lookup(self, key):
+        raise KeyError(key)
+
+    def insert(self, key, value):
+        if self.root is None:
+            self.root = LeafNode(key, value)
+        else:
+            self.root = self._insert(self.root, key, value)
+
+    def _insert(self, node, key, value):
+        if node is None:
+            return LeafNode(key, value)
+        elif isinstance(node, LeafNode):
+            if node.key == key:
+                return LeafNode(key, value)
+            else:
+                raise Exception('internal error')
+        else:
+            if node.child2 is None:
+        
+    def remove(self, key):
+        raise KeyError(key)
--- btree_tests.py
+++ btree_tests.py
@@ -30,3 +30,68 @@ class IndexNodeTests(unittest.TestCase):
     def test_has_second_child(self):
         self.assertEqual(self.node.child2, self.leaf2)
 
+
+class BinarySearchTreeTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tree = btree.BinarySearchTree()
+
+    def test_tree_is_empty(self):
+        self.assertEqual(self.tree.root, None)
+        
+    def test_lookup_for_missing_key_raises_error(self):
+        self.assertRaises(KeyError, self.tree.lookup, 'foo')
+        
+    def test_insert_inserts_key(self):
+        self.tree.insert('foo', 'bar')
+        self.assertEqual(self.tree.lookup('foo'), 'bar')
+
+    def test_insert_replaces_value_for_existing_key(self):
+        self.tree.insert('foo', 'foo')
+        self.tree.insert('foo', 'bar')
+        self.assertEqual(self.tree.lookup('foo'), 'bar')
+
+    def test_remove_of_missing_key_raises_keyerror(self):
+        self.assertRaises(KeyError, self.tree.remove, 'foo')
+
+    def test_remove_removes_key(self):
+        self.tree.insert('foo', 'bar')
+        self.tree.remove('foo')
+        self.assertRaises(KeyError, self.tree.lookup, 'foo')
+
+    def keys(self, node):
+        if node is not None:
+            if isinstance(node, btree.LeafNode):
+                yield key
+            else:
+                for key in self.keys(node.child1):
+                    yield key
+                for key in self.keys(node.child2):
+                    yield key
+
+    def max_key(self, node):
+        return max(self.keys(node))
+
+    def min_key(self, node):
+        return min(self.keys(node))
+
+    def proper_search_tree(self, node):
+        if node is None:
+            return True
+        if isinstance(node, btree.LeafNode):
+            return True
+        if not self.proper_search_tree(node.child1):
+            return False
+        if not self.proper_search_tree(node.child2):
+            return False
+        if self.child2 is None:
+            return True
+        return self.max_key(node.child1) < self.min_key(node.child2)
+
+    def test_insert_many_respects_ordering_requirement(self):
+        for i in range(100):
+            key = str(i)
+            value = key
+            self.tree.insert(key, value)
+            self.assert_(self.proper_search_tree(self.tree.root))
+
