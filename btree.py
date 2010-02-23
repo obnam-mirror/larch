d4c91635069d53b1724d2ca3210339c7f6ed3ee9
Lars Wirzenius <liw@liw.fi>
2010-02-23 18:34:37 +1300

    Implement index node.

--- btree.py
+++ btree.py
@@ -3,3 +3,10 @@ class LeafNode(object):
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        
+
+class IndexNode(object):
+
+    def __init__(self, child1, child2):
+        self.child1 = child1
+        self.child2 = child2
--- btree_tests.py
+++ btree_tests.py
@@ -16,3 +16,17 @@ class LeafNodeTests(unittest.TestCase):
     def test_has_value(self):
         self.assertEqual(self.leaf.value, self.value)
 
+
+class IndexNodeTests(unittest.TestCase):
+
+    def setUp(self):
+        self.leaf1 = btree.LeafNode('foo', 'foo')
+        self.leaf2 = btree.LeafNode('bar', 'bar')
+        self.node = btree.IndexNode(self.leaf1, self.leaf2)
+
+    def test_has_first_child(self):
+        self.assertEqual(self.node.child1, self.leaf1)
+        
+    def test_has_second_child(self):
+        self.assertEqual(self.node.child2, self.leaf2)
+
