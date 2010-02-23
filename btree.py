6c7c1dff03b6b96b87581ef4cf797e611b97e457
Lars Wirzenius <liw@liw.fi>
2010-02-23 19:38:09 +1300

    Implement the simplest possible binary search tree.

--- btree.py
+++ btree.py
@@ -1,13 +1,8 @@
-class LeafNode(object):
+class Node(object):
 
-    def __init__(self, key, value):
+    def __init__(self, key, value, child1, child2):
         self.key = key
         self.value = value
-        
-
-class IndexNode(object):
-
-    def __init__(self, child1, child2):
         self.child1 = child1
         self.child2 = child2
         
@@ -18,24 +13,47 @@ class BinarySearchTree(object):
         self.root = None
         
     def lookup(self, key):
-        raise KeyError(key)
+        return self._lookup(self.root, key)
+
+    def _lookup(self, node, key):
+        if node is None:
+            raise KeyError(key)
+        elif key == node.key:
+            if node.value is None:
+                raise KeyError(key)
+            return node.value
+        elif key < node.key:
+            return self._lookup(node.child1, key)
+        else:
+            return self._lookup(node.child2, key)
 
     def insert(self, key, value):
-        if self.root is None:
-            self.root = LeafNode(key, value)
-        else:
-            self.root = self._insert(self.root, key, value)
+        self.root = self._insert(self.root, key, value)
 
     def _insert(self, node, key, value):
         if node is None:
-            return LeafNode(key, value)
-        elif isinstance(node, LeafNode):
-            if node.key == key:
-                return LeafNode(key, value)
-            else:
-                raise Exception('internal error')
+            return Node(key, value, None, None)
+        elif node.key == key:
+            return Node(key, value, node.child1, node.child2)
+        elif key < node.key:
+            new_child = self._insert(node.child1, key, value)
+            return Node(node.key, node.value, new_child, node.child2)
         else:
-            if node.child2 is None:
+            new_child = self._insert(node.child2, key, value)
+            return Node(node.key, node.value, node.child1, new_child)
         
     def remove(self, key):
-        raise KeyError(key)
+        self.root = self._remove(self.root, key)
+        
+    def _remove(self, node, key):
+        if node is None:
+            raise KeyError(key)
+        elif key == node.key:
+            return Node(key, None, node.child1, node.child2)
+        elif key < node.key:
+            new_child = self._remove(node.child1, key)
+            return Node(node.key, node.value, new_child, node.child2)
+        else:
+            new_child = self._remove(node.child2, key)
+            return Node(node.key, node.value, node.child1, new_child)
+
--- btree_tests.py
+++ btree_tests.py
@@ -1,34 +1,7 @@
+import random
 import unittest
 
 import btree
-
-
-class LeafNodeTests(unittest.TestCase):
-
-    def setUp(self):
-        self.key = 'key'
-        self.value = 'value'
-        self.leaf = btree.LeafNode(self.key, self.value)
-
-    def test_has_key(self):
-        self.assertEqual(self.leaf.key, self.key)
-
-    def test_has_value(self):
-        self.assertEqual(self.leaf.value, self.value)
-
-
-class IndexNodeTests(unittest.TestCase):
-
-    def setUp(self):
-        self.leaf1 = btree.LeafNode('foo', 'foo')
-        self.leaf2 = btree.LeafNode('bar', 'bar')
-        self.node = btree.IndexNode(self.leaf1, self.leaf2)
-
-    def test_has_first_child(self):
-        self.assertEqual(self.node.child1, self.leaf1)
-        
-    def test_has_second_child(self):
-        self.assertEqual(self.node.child2, self.leaf2)
 
 
 class BinarySearchTreeTests(unittest.TestCase):
@@ -59,39 +32,36 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.tree.remove('foo')
         self.assertRaises(KeyError, self.tree.lookup, 'foo')
 
-    def keys(self, node):
-        if node is not None:
-            if isinstance(node, btree.LeafNode):
-                yield key
-            else:
-                for key in self.keys(node.child1):
-                    yield key
-                for key in self.keys(node.child2):
-                    yield key
-
-    def max_key(self, node):
-        return max(self.keys(node))
-
-    def min_key(self, node):
-        return min(self.keys(node))
-
     def proper_search_tree(self, node):
         if node is None:
             return True
-        if isinstance(node, btree.LeafNode):
-            return True
-        if not self.proper_search_tree(node.child1):
-            return False
-        if not self.proper_search_tree(node.child2):
-            return False
-        if self.child2 is None:
-            return True
-        return self.max_key(node.child1) < self.min_key(node.child2)
+        if node.child1 is not None:
+            if node.child1.key > node.key:
+                return False
+        if node.child2 is not None:
+            if node.child2.key < node.key:
+                return False
+        return True
 
     def test_insert_many_respects_ordering_requirement(self):
-        for i in range(100):
+        ints = range(100)
+        random.shuffle(ints)
+        for i in ints:
             key = str(i)
             value = key
             self.tree.insert(key, value)
+            self.assertEqual(self.tree.lookup(key), value)
+            self.assert_(self.proper_search_tree(self.tree.root))
+
+    def test_remove_many_works(self):
+        ints = range(100)
+        random.shuffle(ints)
+        for i in ints:
+            key = str(i)
+            value = key
+            self.tree.insert(key, value)
+            self.assertEqual(self.tree.lookup(key), value)
+            self.tree.remove(key)
+            self.assertRaises(KeyError, self.tree.lookup, key)
             self.assert_(self.proper_search_tree(self.tree.root))
 
