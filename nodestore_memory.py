cc211c0514f4cacf881e9f68878ec35c33417576
Lars Wirzenius <liw@liw.fi>
2010-04-05 16:49:34 +1200

    Fix NodeStoreMemory.put_test to check against re-putting a node.

--- nodestore_memory.py
+++ nodestore_memory.py
@@ -16,6 +16,8 @@ class NodeStoreMemory(btree.NodeStore):
     def put_node(self, node_id, encoded_node):
         if len(encoded_node) > self.node_size:
             raise btree.NodeTooBig(node_id, len(encoded_node))
+        if node_id in self.nodes:
+            raise btree.NodeExists(node_id)
         self.nodes[node_id] = encoded_node
         
     def get_node(self, node_id):
