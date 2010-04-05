86a73cb8265e67561fe43f561011ca73e1dc86b8
Lars Wirzenius <liw@liw.fi>
2010-04-05 16:33:46 +1200

    Add NodeStore and NodeStoreTests classes.
    These do nothing, they are just there to provide an API and 
    shared tests for implementations of the API.

--- btree.py
+++ btree.py
@@ -264,3 +264,106 @@ class BTree(object):
         pairs.sort()
         assert pairs
         return IndexNode(pairs)
+        
+        
+class NodeStore(object): # pragma: no cover
+
+    '''Abstract base class for storing nodes externally.
+    
+    The BTree class itself does not handle external storage of nodes.
+    Instead, it is given an object that implements the API in this
+    class. An actual implementation might keep nodes in memory, or
+    store them on disk using a filesystem, or a database.
+    
+    Node stores deal with nodes as byte strings: the BTree class
+    encodes them before handing them to the store, and decodes them
+    when it gets them from the store.
+    
+    Each node has an identifier that is unique within the tree.
+    The identifier is an integer, and the BTree makes the following
+    guarantees about it:
+    
+    * it is a non-negative integer
+    * new nodes are assigned the next consecutive one
+    * it is never re-used
+    
+    Further, the BTree makes the following guarantees about the encoded
+    nodes:
+    
+    * they have a strict upper size limit
+    * the tree attempts to fill nodes as close to the limit as possible
+    
+    The size limit is given to the node store at initialization time.
+    It is accessible via the node_size property. Implementations of
+    this API must handle that in some suitable way, preferably by 
+    inheriting from this class and calling its initializer.
+    
+    '''
+    
+    def __init__(self, node_size):
+        self.node_size = node_size
+        
+    def put_node(self, node_id, encoded_node):
+        '''Put a new node into the store.'''
+        
+    def get_node(self, node_id):
+        '''Return a node from the store.
+        
+        Raise the NodeMissing exception if the node is not in the
+        store (has never been, or has been removed). Raise other
+        errors as suitable.
+        
+        '''
+        
+    def remove_node(self, node_id):
+        '''Remove a node from the store.'''
+        
+    def list_nodes(self):
+        '''Return list of ids of all nodes in store.'''
+
+
+class NodeStoreTests(object): # pragma: no cover
+
+    '''Re-useable tests for NodeStore implementations.
+    
+    The NodeStore base class can't be usefully instantiated itself.
+    Instead you are supposed to sub-class it and implement the API in
+    a suitable way for yourself.
+    
+    This class implements a number of tests that the API implementation
+    must pass. The implementation's own test class should inherit from
+    this class, and unittest.TestCase.
+    
+    The test sub-class should define a setUp method that sets the following:
+    
+    * self.ns to an instance of the API implementation sub-class
+    * self.node_size to the node size
+    
+    '''
+    
+    def test_sets_node_size(self):
+        self.assertEqual(self.ns.node_size, self.node_size)
+        
+    def test_has_no_node_zero_initially(self):
+        self.assertRaises(NodeMissing, self.ns.get_node, 0)
+
+    def test_find_no_nodes_initially(self):
+        self.assertEqual(self.ns.find_nodes(), [])
+        
+    def test_puts_and_gets_same(self):
+        encoded = LeafNode().encode()
+        self.ns.put(0, encoded)
+        self.assertEqual(self.ns.get_node(0), encoded)
+        
+    def test_removes_node(self):
+        encoded = LeafNode().encode()
+        self.ns.put(0, encoded)
+        self.ns.remove_node(0)
+        self.assertRaises(NodeMissing, self.ns.get_node, 0)
+        self.assertEqual(self.ns.find_nodes(), [])
+
+    def test_finds_node_zero(self):
+        encoded = LeafNode().encode()
+        self.ns.put(0, encoded)
+        self.assertEqual(self.ns.find_nodes(), [0])
+
