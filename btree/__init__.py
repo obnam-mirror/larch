5afebabcecbb331c64e0c7e0dd3c19f037abb71f
Lars Wirzenius <liw@liw.fi>
2010-05-03 19:35:51 +1200

    Start growing a forest.

--- btree/__init__.py
+++ btree/__init__.py
@@ -1,6 +1,7 @@
 from nodes import LeafNode, IndexNode
 from codec import NodeCodec
 from tree import BTree, KeySizeMismatch
+from forest import Forest
 from nodestore import (NodeStore, NodeStoreTests, NodeMissing, NodeTooBig, 
                        NodeExists)
 from intset import IntSet
