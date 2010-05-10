9b1716192ff2d3087c1549e4a936eaacf2bd0995
Lars Wirzenius <liw@liw.fi>
2010-05-10 23:54:28 +1200

    Remove unused module intset.

--- btree/__init__.py
+++ btree/__init__.py
@@ -23,7 +23,5 @@ from tree import BTree, KeySizeMismatch
 from forest import Forest
 from nodestore import (NodeStore, NodeStoreTests, NodeMissing, NodeTooBig, 
                        NodeExists)
-from intset import IntSet
 from nodestore_disk import NodeStoreDisk
 from nodestore_memory import NodeStoreMemory
-
