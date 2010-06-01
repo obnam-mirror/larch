55bf7053cb2f72669caa0101d156daec42b0cb7a
Lars Wirzenius <liw@liw.fi>
2010-06-02 07:53:22 +1200

    Optimize leaf_size a bit.
    Thanks to Richard Braakman for suggesting this. It turns out
    doing len(''.join([x for ...])) is faster than doing
    sum([len(x) for ...]), who would have thought.

--- btree/codec.py
+++ btree/codec.py
@@ -49,7 +49,7 @@ class NodeCodec(object):
         '''Return size of a leaf node with the given pairs.'''
         return (self.leaf_header_size + len(pairs) * self.key_bytes + 
                 len(pairs) * struct.calcsize('I') +
-                sum([len(value) for key, value in pairs]))
+                len(''.join([value for key, value in pairs])))
 
     def leaf_format(self, pairs):
         return ('!4sQI' + ('%ds' % self.key_bytes) * len(pairs) + 
