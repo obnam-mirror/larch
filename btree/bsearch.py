14b23da4aefcda31932d8ceeef8588eb438fd382
Lars Wirzenius <liw@liw.fi>
2010-06-06 12:56:15 +1200

    Use loop instead of recursion.

--- btree/bsearch.py
+++ btree/bsearch.py
@@ -41,25 +41,26 @@ def bsearch(array, key, getkey=None):
     '''
 
     def helper(lo, hi):
-        lokey = getkey(array[lo])
-        hikey = getkey(array[hi])
+        while True:
+            lokey = getkey(array[lo])
+            hikey = getkey(array[hi])
 
-        mid = (lo + hi) / 2
-        midkey = getkey(array[mid])
+            mid = (lo + hi) / 2
+            midkey = getkey(array[mid])
 
-        if key == lokey:
-            return lo, lo
-        elif key == hikey:
-            return hi, hi
-        elif key == midkey:
-            return mid, mid
-        elif (hi-lo) == 1:
-            return lo, hi
-        elif lokey < key < midkey:
-            return helper(lo, mid)
-        else:
-            assert midkey < key < hikey
-            return helper(mid, hi)
+            if key == lokey:
+                return lo, lo
+            elif key == hikey:
+                return hi, hi
+            elif key == midkey:
+                return mid, mid
+            elif (hi-lo) == 1:
+                return lo, hi
+            elif lokey < key < midkey:
+                hi = mid
+            else:
+                assert midkey < key < hikey
+                lo = mid
 
     getkey = getkey or (lambda x: x)
     lo = 0
