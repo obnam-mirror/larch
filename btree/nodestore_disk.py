0161694518947bd15b36016099bbdd9a8d562784
Lars Wirzenius <liw@liw.fi>
2010-06-17 21:26:44 +1200

    Fix refcount-drops-to-zero.
    Unit tests, how I love thee...

--- btree/nodestore_disk.py
+++ btree/nodestore_disk.py
@@ -48,8 +48,9 @@ class RefcountStore(object):
         return self.refcounts[node_id]
 
     def set_refcount(self, node_id, refcount):
-        if refcount == 0 and refcount in self.refcounts:
-            del self.refcounts[node_id]
+        if refcount == 0:
+            if node_id in self.refcounts:
+                del self.refcounts[node_id]
         else:
             self.refcounts[node_id] = refcount
         self.dirty.add(node_id)
