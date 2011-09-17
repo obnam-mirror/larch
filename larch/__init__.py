16954a73a2fab7b2a726a213c7b34bbb485d0522
Lars Wirzenius <liw@liw.fi>
2011-09-17 20:08:10 +0100

    Release version 0.24.

--- larch/__init__.py
+++ larch/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.23'
+__version__ = '0.24'
 
 
 from nodes import FrozenNode, Node, LeafNode, IndexNode
