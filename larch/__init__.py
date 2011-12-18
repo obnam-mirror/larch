c9fdc1912b36efb4ee2720769ee79e384cf65050
Lars Wirzenius <liw@liw.fi>
2011-12-18 16:54:41 +0000

    prepare release 0.26

--- larch/__init__.py
+++ larch/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.25'
+__version__ = '0.26'
 
 
 from nodes import FrozenNode, Node, LeafNode, IndexNode
